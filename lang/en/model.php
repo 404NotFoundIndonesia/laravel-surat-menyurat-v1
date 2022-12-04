435d70794153d6e55add0d691ea3dc37435eccc3
M. Iqbal Effendi <iqbaleff214@gmail.com>
2022-12-04 13:29:54 +0800

    add letter model locale

---
+++ lang/en/model.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'letter' => [
+        'agenda_number' => 'Agenda Number',
+        'from' => 'Sender',
+        'to' => 'Receiver',
+        'reference_number' => 'Reference Number',
+        'letter_date' => 'Letter Date',
+        'received_date' => 'Received Date',
+        'description' => 'Brief Contents',
+        'note' => 'Note',
+        'dispose' => 'Dispose',
+        'attachment' => 'Attachment',
+        'status' => [
+            'all' => 'All',
+            'disposed' => 'Disposed',
+            'not_disposed' => 'Not Disposed',
+        ],
+    ],
+    'general' => [
+        'created_at' => 'Created At',
+        'updated_at' => 'Updated At',
+        'deleted_at' => 'Deleted At',
+        'created_by' => 'Created By',
+    ],
+];
---
+++ lang/id/model.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'letter' => [
+        'agenda_number' => 'Nomor Agenda',
+        'from' => 'Pengirim',
+        'to' => 'Penerima',
+        'reference_number' => 'Nomor Surat',
+        'letter_date' => 'Tanggal Surat',
+        'received_date' => 'Tanggal Diterima',
+        'description' => 'Ringkasan',
+        'note' => 'Keterangan',
+        'dispose' => 'Disposisi Surat',
+        'attachment' => 'Lampiran',
+        'status' => [
+            'all' => 'Semua',
+            'disposed' => 'Ada Disposisi',
+            'not_disposed' => 'Belum Disposisi',
+        ],
+    ],
+    'general' => [
+        'created_at' => 'Dibuat pada',
+        'updated_at' => 'Diperbarui pada',
+        'deleted_at' => 'Dihapus pada',
+        'created_by' => 'Dibuat oleh',
+    ],
+];
