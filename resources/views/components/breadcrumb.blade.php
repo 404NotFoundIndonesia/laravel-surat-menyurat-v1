2860035b78e8325e94008e38569974448513be79
M. Iqbal Effendi <iqbaleff214@gmail.com>
2022-12-04 11:57:44 +0800

    add slot for Breadcrumb component

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -1,13 +1,18 @@
 <div>
     @if(count($values))
-    <h4 class="fw-bold py-3 mb-4">
-        @foreach($values as $value)
-            @if($loop->last)
-                {{ $value }}
-            @else
-                <span class="text-muted fw-light">{{ $value }} /</span>
-            @endif
-        @endforeach
-    </h4>
+        <div class="d-flex justify-content-between flex-column flex-sm-row">
+            <h4 class="fw-bold py-3 mb-4">
+                @foreach($values as $value)
+                    @if($loop->last)
+                        {{ $value }}
+                    @else
+                        <span class="text-muted fw-light">{{ $value }} /</span>
+                    @endif
+                @endforeach
+            </h4>
+            <div class="py-3">
+                {{ $slot }}
+            </div>
+        </div>
     @endif
 </div>
