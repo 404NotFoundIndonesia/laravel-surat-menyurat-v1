1c35d852502137241a44be1120cc32407adcb49a
M. Iqbal Effendi <iqbaleff214@gmail.com>
2022-12-05 08:26:37 +0800

    user database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,13 +12,10 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(
+            UserSeeder::class,
+        );
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Administrator',
+            'email' => 'admin@admin.com',
+            'phone' => '082121212121',
+            'password' => Hash::make('admin'),
+            'role' => Role::ADMIN->status(),
+        ]);
+    }
+}
