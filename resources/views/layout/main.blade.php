2c1451855ecc19687b28f40a658e89ed8ed04fd4
M. Iqbal Effendi <iqbaleff214@gmail.com>
2022-12-07 04:13:14 +0800

    logo and favicon

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -27,12 +27,12 @@
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>Dashboard | {{ config('app.name') }}</title>
+    <title>{{ config('app.name') }}</title>
 
     <meta name="description" content=""/>
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{asset('sneat/img/favicon/favicon.ico')}}"/>
+    <link rel="icon" type="image/x-icon" href="{{ asset('logo-black.png') }}"/>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
